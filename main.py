a2cdd82d4254a26638a8c65a5d486b7f4d01e3bf
agregonnn9 <agregonace@gmail.com>
2024-02-01 19:31:46 +0300

    сделал зум только не через pgup pgdown а через кнопки вверх вниз

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import pygame
 import requests
 
-
 WIDTH, HEIGHT = 600, 450
 
+latitude, longitude = 68.97917, 33.09251
+zoom = 10
 
 def show_yandex_map(latitude, longitude, zoom):
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('Яндекс Карты')
 
-    response = requests.get(
-        f'https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}&z={zoom}&l=map'
-    )
-
-    with open('map.png', 'wb') as f:
-        f.write(response.content)
-
-    map_image = pygame.image.load('map.png')
-
-    screen.blit(map_image, (0, 0))
-
     running = True
     while running:
+        response = requests.get(
+            f'https://static-maps.yandex.ru/1.x/?ll={longitude},{latitude}&z={zoom}&l=map'
+        )
+
+        with open('map.png', 'wb') as f:
+            f.write(response.content)
+
+        map_image = pygame.image.load('map.png')
+
+        screen.blit(map_image, (0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    zoom += 1
+                elif event.key == pygame.K_DOWN:
+                    zoom -= 1
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
 
         pygame.display.update()
 
     pygame.quit()
-#
 
-show_yandex_map(68.97917, 33.09251, 10)
+show_yandex_map(latitude, longitude, zoom)
