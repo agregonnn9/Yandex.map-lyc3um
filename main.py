01fc3df5ced14c01fbc6ccb2a8598f9cd9822d7c
Leonid Strukov <leostrukov@ya.ru>
2024-03-13 22:03:56 +0300

    Добавил возможность передвигаться по карте с помощью клавиш WASD, по стрелочкам не получилось, так как на кнопки PgUp и PgDown сфокусированы.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt
 import requests
 from ui import Ui_MainWindow
 import os
@@ -11,15 +12,20 @@ class YandexMapApp(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.pixmap = None
-        self.latitude, self.longitude = 68.97917, 33.09251
         self.zoom = 10
+        self.step_for_zoom = {0: 10.0, 1: 5.0, 2: 4.0, 3: 2.0, 4: 2.0, 5: 1.5, 6: 1.5, 7: 1.0, 8: 1.0, 9: 0.1, 10: 0.1,
+                              11: 0.01, 12: 0.01, 13: 0.01, 14: 0.001, 15: 0.001, 16: 0.001, 17: 0.001, 18: 0.0001,
+                              19: 0.0001, 20: 0.0001, 21: 0.0001}
+        self.step = self.step_for_zoom[self.zoom]
+
+        self.latitude, self.longitude = 68.97917, 33.09251
         self.show_yandex_map()
         self.PgUp.clicked.connect(self.zoom_pl)
         self.PgDown.clicked.connect(self.zoom_mn)
 
     def show_yandex_map(self):
         params = {
-            'll': f'{self.latitude},{self.longitude}',
+            'll': f'{self.longitude},{self.latitude}',
             'z': self.zoom,
             'l': 'map'
         }
@@ -43,6 +49,31 @@ class YandexMapApp(QMainWindow, Ui_MainWindow):
         if self.zoom != 0:
             self.zoom -= 1
             self.show_yandex_map()
+
+    def keyPressEvent(self, event):
+        self.step = self.step_for_zoom[self.zoom]
+        if event.key() == Qt.Key_W:
+            if self.latitude + self.step <= 90:
+                self.latitude += self.step
+            else:
+                self.latitude = 90
+        elif event.key() == Qt.Key_S:
+            if self.latitude - self.step >= -90:
+                self.latitude -= self.step
+            else:
+                self.latitude = -90
+        elif event.key() == Qt.Key_A:
+            if self.longitude - self.step >= -180:
+                self.longitude -= self.step
+            else:
+                self.longitude = -180
+
+        elif event.key() == Qt.Key_D:
+            if self.longitude + self.step <= 180:
+                self.longitude += self.step
+            else:
+                self.longitude = 180
+        self.show_yandex_map()
 
 
 def except_hook(cls, exception, traceback):
